8ceaa446ef0f26be3b920ae4d6939785bf9d5a2a
begugla1 <darveter667@gmail.com>
2023-06-10 17:25:32 +0300

    refactoring 9.3

--- user/models.py
+++ user/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
     phone_number = models.CharField('Номер телефона', max_length=100, blank=True, null=True)
-    profile_photo = models.ImageField('Фото профиля', upload_to='media/profile_photos/%Y/%m/%d', blank=True)
+    profile_photo = models.ImageField('Фото профиля', upload_to='profile_photos/%Y/%m/%d', blank=True)
 
     def __str__(self):
         return f'Профиль пользователя {self.user.username}'
